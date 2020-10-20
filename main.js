92fe41fc909ee84de131fbfa23e2af65ace1e99d
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-20 16:14:14 +0200

    tried with while

--- main.js
+++ main.js
@@ -9,17 +9,40 @@ var maxNumber = 100;
 //creo variabile per i multipli
 
 //creo ciclo che stampi numeri da 1 a 100
-for (var i = 1; i <= maxNumber; i++) {
-    var currentNumber = i;
-    // console.log(currentNumber);
-    if (currentNumber % 3 == 0 && currentNumber % 5 == 0 ) {
-        console.log("FizzBuzz " + i);
-    } else if (currentNumber % 5 == 0) {
-        console.log("buzz " + i);
+// for (var i = 1; i <= maxNumber; i++) {
+//     var currentNumber = i;
+//     // console.log(currentNumber);
+//     if (currentNumber % 3 == 0 && currentNumber % 5 == 0 ) {
+//         console.log("FizzBuzz " + i);
+//     } else if (currentNumber % 5 == 0) {
+//         console.log("buzz " + i);
+//
+//     } else if (currentNumber % 3 == 0) {
+//         console.log("Fizz " + i);
+//     } else {
+//         console.log(currentNumber);
+//     }
+// }
 
-    } else if (currentNumber % 3 == 0) {
-        console.log("Fizz " + i);
+//second method
+
+var i = 1;
+while (i <= maxNumber) {
+    var numeroCorrente = i;
+    // console.log(numeroCorrente);
+    //creo variabile per il multiplo di 3
+    var multiplo3 = numeroCorrente % 3 == 0;
+    //creo variabile per il multiplo di 5
+    var multiplo5 = numeroCorrente % 5 == 0;
+    //creo la condizione per la stampa fizzbuzz
+    if (multiplo3 && multiplo5) {
+        console.log("FizzBuzz " + numeroCorrente);
+    } else if (multiplo3) {
+        console.log("Fizz " + numeroCorrente);
+    } else if (multiplo5) {
+        console.log("Buzz " + numeroCorrente);
     } else {
-        console.log(currentNumber);
+        console.log(numeroCorrente);
     }
+    i++
 }
